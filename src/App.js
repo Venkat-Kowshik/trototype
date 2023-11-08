bf896ce7cbc24c70421e5521dd25f52a0c8996ca
Kowshik <kowshikgowrakkavari777@gmail.com>
2023-11-08 15:42:54 +0530

    gta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-
+import Checkout from "./Pages/Checkout";
+import Payment from "./Pages/Payment";
 import AllRoutes from "./AllRoutes";
 import { Footer } from "./Components/Shared/Footer";
 import { NavbarShared } from "./Components/Shared/Navbar";
@@ -11,12 +12,16 @@ import {
   onAuthStateChangedListener,
   updateBookings,
 } from "./Utils/firebase/firebase";
-import { loginUser } from "./Redux/userReducer/userActions";
+import { getUserDetails, loginUser } from "./Redux/userReducer/userActions";
 import { useDispatch, useSelector } from "react-redux";
 import { userReducer } from "./Redux/userReducer/userReducer";
 import { setWindowClick } from "./Redux/windowReducer/windowActions";
+import { LOGOUT_USER } from "./Redux/userReducer/userTypes";
+import { getBookings } from "./Redux/bookingReducer/bookingActions";
+
 function App() {
   const { isAuth, userDetails } = useSelector((state) => state.userReducer);
+  const { userBookings } = useSelector((state) => state.bookingReducer);
   const dispatch = useDispatch();
 
   const handleWindowClicked = () => {
@@ -24,24 +29,16 @@ function App() {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener(async (user) => {
-      if (user) {
-        console.log(user);
-        console.log(user.displayName);
-        await createUserDocumentFromAuth(user, {
-          bookings: [{ id: "adventour@test" }],
-        });
-        const data = getUserDocumentFromAuth(user);
-        data.then((res) => {
-          console.log(res.data());
-          dispatch(loginUser({ ...res.data(), uid: user.uid }));
-        });
-        console.log(user);
-      }
-    });
+    const token = localStorage.getItem("token") || "";
+    if (token) {
+      dispatch(getUserDetails(token));
+      dispatch(getBookings(token));
+      console.log(userDetails);
+    } else {
+      dispatch({ type: LOGOUT_USER });
+    }
+  }, [isAuth]);
 
-    return unsubscribe;
-  }, [dispatch]);
   return (
     <div>
       <div class="background">
@@ -52,6 +49,7 @@ function App() {
         <span></span>
         <span></span>
       </div>
+
       <NavbarShared />
       <AllRoutes />
       <Footer />
